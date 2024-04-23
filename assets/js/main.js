5c91c906d150e65fc35d21da443c449a5ed0ee0f
Vee <ifebuchejulia@gmail.com>
2024-04-23 10:50:47 +0100

    content update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,13 @@
+var crsr = document.querySelector("#cursor");
+var blur = document.querySelector("#cursor-blur");
+
+document.addEventListener("mousemove", function (dets) {
+  crsr.style.left = dets.x + "px";
+  crsr.style.top = dets.y + "px";
+  blur.style.left = dets.x - 250 + "px";
+  blur.style.top = dets.y - 250 + "px";
+});
+
 /*=============== SHOW MENU ===============*/
 const navMenu = document.getElementById('nav-menu'),
       navToggle = document.getElementById('nav-toggle'),
